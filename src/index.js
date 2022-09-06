3dff80a5a9e71a4ddc12a73e86c51ba0a95da364
Fabricio <fgonzalezguasque@gmail.com>
2022-09-06 15:42:24 -0300

    Ejercicio 2

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+// - Tu nombre (string)
+// - Tu edad (number)
+// - ¿Eres desarrollador? (boolean)
+// - Tu fecha de nacimiento (Date)
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const myData = ["Fabricio", 36, true, new Date(1986,3,17)]
+
+const favouriteBook = {
+    title : "El viento por la cerradura",
+    author : "Stephen King",
+    date : new Date(2012,1,22),
+    url : "https://books.google.com.uy/books/about/El_viento_por_la_cerradura.html?id=xUTfigJoAZsC&redir_esc=y"
+}
